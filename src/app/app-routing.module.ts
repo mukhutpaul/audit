d0db4e4724239c5e1eeb07472a106d9724e3481a
jp <mukhutjp@gmail.com>
2023-06-23 14:55:34 +0400

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,29 @@ import { GestionMissionComponent } from './pages/gestion-mission/gestion-mission
 import { FUniteComponent } from './Formulaires/funite/funite.component';
 import { FMissionComponent } from './Formulaires/fmission/fmission.component';
 import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { CategorieComponent } from './pages/categorie/categorie.component';
+import { FinancierComponent } from './pages/financier/financier.component';
+import { AuditComponent } from './pages/audit/audit.component';
+import { DocumentComponent } from './pages/document/document.component';
+import { DetailAuditComponent } from './pages/detail-audit/detail-audit.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'unite', component: GestionUniteComponent },
   { path: 'mission', component: GestionMissionComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'categorie', component: CategorieComponent },
+  { path: 'financier', component: FinancierComponent },
+  { path: 'audit', component: AuditComponent },
+  { path: 'Document', component: DocumentComponent },
 
   { path: 'funite', component: FUniteComponent },
   { path: 'fmission', component: FMissionComponent },
+  { path: 'detailaudit', component: DetailAuditComponent},
+  { path: 'profile', component: ProfileComponent},
 
 ];
 
